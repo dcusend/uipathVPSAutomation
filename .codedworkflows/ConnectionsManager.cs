33529843f003d3a1b18dd2da6eed7440452176cd
Imtiaz-bitbucket <imtiazster@gmail.com>
2024-05-23 22:08:57 -0400

    Added Test Cases for Training.

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace VPSAutomation
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
